bbdc5e9e80d1cdb4c03dd33e9f78851bf833a520
jotd <jotd@orange.fr>
2021-05-14 23:37:16 +0200

    quit support for Black Danw Rebirth

---
+++ B/BlackDawnRebirthHDDev/usr/source/BlackDawnRebirthHD.s
@@ -0,0 +1,307 @@
+;*---------------------------------------------------------------------------
+;  :Program.	BoppinHD.asm
+;  :Contents.	Slave for "Boppin"
+;  :Author.	JOTD, from Wepl sources
+;  :Original	v1 
+;  :Version.	$Id: BoppinHD.asm 1.2 2002/02/08 01:18:39 wepl Exp wepl $
+;  :History.	%DATE% started
+;  :Requires.	-
+;  :Copyright.	Public Domain
+;  :Language.	68000 Assembler
+;  :Translator.	Devpac 3.14, Barfly 2.9
+;  :To Do.
+;---------------------------------------------------------------------------*
+
+	INCDIR	Include:
+	INCLUDE	whdload.i
+	INCLUDE	whdmacros.i
+	INCLUDE	lvo/dos.i
+
+;CHIP_ONLY
+	IFD BARFLY
+	OUTPUT	"BlackDawnRebirth.slave"
+	IFND	CHIP_ONLY
+	BOPT	O+				;enable optimizing
+	BOPT	OG+				;enable optimizing
+	ENDC
+	BOPT	ODd-				;disable mul optimizing
+	BOPT	ODe-				;disable mul optimizing
+	BOPT	w4-				;disable 64k warnings
+	BOPT	wo-			;disable optimizer warnings
+	SUPER
+	ENDC
+
+;============================================================================
+
+
+	IFD	CHIP_ONLY
+HRTMON
+CHIPMEMSIZE	= $200000
+FASTMEMSIZE	= $0000
+	ELSE
+BLACKSCREEN
+CHIPMEMSIZE	= $100000
+FASTMEMSIZE	= $100000
+	ENDC
+
+NUMDRIVES	= 1
+WPDRIVES	= %0000
+
+;DISKSONBOOT
+;DOSASSIGN
+;INITAGA
+HDINIT
+IOCACHE		= 10000
+;MEMFREE	= $200
+;NEEDFPU
+;SETPATCH
+;STACKSIZE = 10000
+BOOTDOS
+CACHE
+NO68020
+
+slv_Version	= 17
+slv_Flags	= WHDLF_NoError|WHDLF_Examine|WHDLF_ClearMem
+slv_keyexit	= $5D	; num '*'
+
+	include	whdload/kick31.s
+
+;============================================================================
+
+	IFD BARFLY
+	DOSCMD	"WDate  >T:date"
+	ENDC
+
+DECL_VERSION:MACRO
+	dc.b	"1.0"
+	IFD BARFLY
+		dc.b	" "
+		INCBIN	"T:date"
+	ENDC
+	IFD	DATETIME
+		dc.b	" "
+		incbin	datetime
+	ENDC
+	ENDM
+	dc.b	"$","VER: slave "
+	DECL_VERSION
+	dc.b	0
+
+
+slv_name		dc.b	"Black Dawn Rebirth"
+	IFD	CHIP_ONLY
+	dc.b	" (DEBUG/CHIP MODE)"
+	ENDC
+			dc.b	0
+slv_copy		dc.b	"2019 Doublesided Games",0
+slv_info		dc.b	"adapted by JOTD",10,10
+		dc.b	"Version "
+		DECL_VERSION
+		dc.b	0
+slv_CurrentDir:
+	dc.b	"data",0
+
+intro:
+	dc.b	"intro",0
+program:
+	dc.b	"BD",0
+args		dc.b	10
+args_end
+	dc.b	0
+slv_config
+	dc.b    "C3:B:skip introduction;"
+	dc.b	0
+
+; version xx.slave works
+
+	dc.b	"$","VER: slave "
+	DECL_VERSION
+	dc.b	0
+	EVEN
+
+
+_bootdos
+		clr.l	$0.W
+
+    move.l  _resload(pc),a2
+    lea tag(pc),a0
+    jsr (resload_Control,a2)
+    
+	; saves registers (needed for BCPL stuff, global vector, ...)
+
+		lea	(_saveregs,pc),a0
+		movem.l	d1-d7/a1-a2/a4-a6,(a0)
+		lea	_stacksize(pc),a2
+		move.l	4(a7),(a2)
+
+		move.l	_resload(pc),a2		;A2 = resload
+
+	
+	;open doslib
+		lea	(_dosname,pc),a1
+		move.l	(4),a6
+		jsr	(_LVOOldOpenLibrary,a6)
+		move.l	d0,a6			;A6 = dosbase
+        
+;       IFD CHIP_ONLY
+;       movem.l a6,-(a7)
+;		move.l	$4.w,a6
+;       move.l  #$02268,d0
+;       move.l  #MEMF_CHIP,d1
+;       jsr _LVOAllocMem(a6)
+;       movem.l (a7)+,a6
+;       ENDC
+
+    
+        move.l  skip_intro(pc),d0
+        bne.b   .skip
+	;load exe
+		lea	intro(pc),a0
+		lea	args(pc),a1
+		moveq	#args_end-args,d0
+		sub.l   a5,a5
+		bsr	load_exe
+.skip
+	;load exe
+		lea	program(pc),a0
+		lea	args(pc),a1
+		moveq	#args_end-args,d0
+		sub.l   a5,a5
+		bsr	load_exe
+	;quit
+_quit		pea	TDREASON_OK
+		move.l	(_resload,pc),a2
+		jmp	(resload_Abort,a2)
+
+; < d7: seglist (APTR)
+
+patch_main
+	bsr	get_version
+
+	lea	pl_main(pc),a0
+.original    
+    IFD    CHIP_ONLY
+    move.l  d7,a1
+    add.l  a1,a1
+    add.l  a1,a1
+    addq.l  #4,a1
+    move.l  a1,$100.W
+    ENDC
+    
+    move.l  d7,a1
+	jsr	resload_PatchSeg(a2)
+.skip
+	rts
+
+
+
+
+; apply on SEGMENTS
+pl_main
+    PL_START
+
+    ;PL_PS   $40918,set_copper
+    PL_END
+ 
+ set_copper:
+get_version:
+	movem.l	d1/a0/a1,-(a7)
+	lea	program(pc),A0
+	move.l	_resload(pc),a2
+	jsr	resload_GetFileSize(a2)
+
+	cmp.l	#297224,D0
+    bra.b   .original       ; FORCED
+    
+	pea	TDREASON_WRONGVER
+	move.l	_resload(pc),-(a7)
+	addq.l	#resload_Abort,(a7)
+	rts
+
+.original
+	moveq	#1,d0
+    bra.b   .out
+    nop
+
+
+.out
+	movem.l	(a7)+,d1/a0/a1
+	rts
+
+
+
+; < a0: program name
+; < a1: arguments
+; < d0: argument string length
+; < a5: patch routine (0 if no patch routine)
+
+
+load_exe:
+	movem.l	d0-a6,-(a7)
+	move.l	d0,d2
+	move.l	a0,a3
+	move.l	a1,a4
+	move.l	a0,d1
+	jsr	(_LVOLoadSeg,a6)
+
+	move.l	d0,d7			;D7 = segment
+	beq	.end			;file not found
+
+	;patch here
+	cmp.l	#0,A5
+	beq.b	.skip
+	movem.l	d2/d7/a4,-(a7)
+
+
+	jsr	(a5)
+	bsr	_flushcache
+	movem.l	(a7)+,d2/d7/a4
+.skip
+	;call
+	move.l	d7,a3
+	add.l	a3,a3
+	add.l	a3,a3
+
+	move.l	a4,a0
+
+	movem.l	d7/a6,-(a7)
+
+	move.l	d2,d0			; argument string length
+	move.l	_stacksize(pc),-(a7)	; original stack format
+	movem.l	(_saveregs,pc),d1-d7/a1-a2/a4-a6	; original registers (BCPL stuff)
+	jsr	(4,a3)		; call program
+	addq.l	#4,a7
+
+	movem.l	(a7)+,d7/a6
+
+	;remove exe
+
+	move.l	d7,d1
+	jsr	(_LVOUnLoadSeg,a6)
+
+	movem.l	(a7)+,d0-a6
+	rts
+
+.end
+	jsr	(_LVOIoErr,a6)
+	move.l	a3,-(a7)
+	move.l	d0,-(a7)
+	pea	TDREASON_DOSREAD
+	move.l	(_resload,pc),-(a7)
+	add.l	#resload_Abort,(a7)
+	rts
+
+_saveregs
+		ds.l	16,0
+_stacksize
+		dc.l	0
+
+tag		dc.l	WHDLTAG_CUSTOM3_GET
+skip_intro	dc.l	0
+
+		dc.l	0
+		dc.l	0
+
+;============================================================================
+
+	END
