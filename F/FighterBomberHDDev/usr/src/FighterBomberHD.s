3e12022d0e668685ba4b1bf2e0f602649a887738
Jean-François Fabre <jotd666@gmail.com>
2022-11-21 23:19:20 +0100

    slave running, reloc to add now

--- F/FighterBomberHDDev/usr/src/FighterBomberHD.s
+++ F/FighterBomberHDDev/usr/src/FighterBomberHD.s
@@ -12,16 +12,19 @@
 
 	INCDIR	sys:include/
 	INCLUDE	whdload.i
+	INCLUDE	whdmacros.i
 
+	IFD		BARFLY
 	OUTPUT	sys:fighterbomber/fighter.slave
 	;OPT	O+ OG+			;enable optimizing
-
+	ENDC
+	
 ;======================================================================
 
 base
 		SLAVE_HEADER		;ws_Security + ws_ID
-		dc.w	13		;ws_Version
-		dc.w	WHDLF_Disk|WHDLF_NoError|WHDLF_EmulTrap	;ws_flags
+		dc.w	17		;ws_Version
+		dc.w	WHDLF_NoError|WHDLF_EmulTrap	;ws_flags
 		dc.l	$b2000		;ws_BaseMemSize
 		dc.l	0		;ws_ExecInstall
 		dc.w	Start-base	;ws_GameLoader
@@ -33,18 +36,42 @@ _keyexit	dc.b	$46		;ws_keyexit = Del
 		dc.w	_name-base	;ws_name
 		dc.w	_copy-base	;ws_copy
 		dc.w	_info-base	;ws_info
+	dc.w	0                       ;ws_kickname
+	dc.l	0                       ;ws_kicksize
+	dc.w	0                       ;ws_kickcrc
+	dc.w	_config-base		;ws_config
+
+
+DECL_VERSION:MACRO
+	dc.b	"2.0"
+	IFD BARFLY
+		dc.b	" "
+		INCBIN	"T:date"
+	ENDC
+	IFD	DATETIME
+		dc.b	" "
+		incbin	datetime
+	ENDC
+	ENDM
 
 _name	dc.b	'-> F I G H T E R  B O M B E R <-',0
 _copy	dc.b	'1989 Activision',0
 _info	dc.b	'-----------------------',10
 	dc.b	'Installed and fixed by',10
-	dc.b	'Galahad of Fairlight',10
-	dc.b	'Version 1.0 (01.05.2002)',10
+	dc.b	'Galahad of Fairlight & JOTD',10
+	dc.b	'Version '
+	DECL_VERSION
+	dc.b	10
 	dc.b	'------------------------',10
 	dc.b	'Thanks to Slinka & Yoyo',10
 	dc.b	'------------------------',0
 save:	dc.b	'FB_Save',0
 log:	dc.b	'FB_PilotsLog',0
+_config
+	dc.b	"C5:B:skip intro;"
+    ;dc.b    "C1:B:infinite ammo;"
+    dc.b	0
+
 	CNOP 0,2
 
 
@@ -73,26 +100,32 @@ file_exists:
 		moveq	#$b,d2
 		moveq	#1,d3				
 		lea	$7e000,a0
-		lea	Loader(pc),a2
-		jsr	(a2)
-		btst	#6,$bfe001
-		bne.s	no_press
-		move.l	#$60000084,$52(a0)	;Skip Intro
-no_press:
-		move.w	#$4ef9,$800(a0)
-		move.l	a2,$802(a0)		;Boot Loader patched
-		lea	patch(pc),a1
-		move.l	a1,$9c(a0)		;Patch before intro
-		lea	gamepatch(pc),a1
-		move.l	a1,$fe(a0)		;Game patcher!
-		lea	copylock(pc),a1
-		move.w	#$4ef9,$104(a0)
-		move.l	a1,$106(a0)		;Patch Copylock Routine!
-		jmp	$38(a0)			;Execute Boot Loader
+		bsr		Loader
+		move.l	a0,a1
+		move.l	a1,-(a7)
+		add.l	#$38,(a7)
+		lea		pl_boot(pc),a0
+		move.l	_resload(pc),a2
+		jsr		resload_Patch(a2)
+		rts   ;Execute Boot Loader $7E038
+		
 copylock:
 		move.l	#$8488ffc4,d0		;Copylock Serial Key
 		move.l	d0,$24.w		;Secondary store
 		rts		
+		
+pl_boot:
+	PL_START
+	PL_IFC5
+	PL_L	$52,$60000084
+	PL_ENDIF
+	PL_P	$800,Loader
+	PL_P	$9A,patch			;Patch before intro
+	PL_P	$FC,gamepatch		;Game patcher!
+	PL_P	$104,copylock		;Patch Copylock Routine!
+	PL_END
+	
+	
 ;-----------------------------------
 ;Intro Patch
 patch:
@@ -100,28 +133,15 @@ patch:
 		move.w	#$4e71,$110c4		;Remove move.w sr,-(a7)
 		jmp	$10000
 ;----------------------------------
+
+
 gamepatch:
-		clr.b	$14878			;Fix Address Error
-		lea	Loader2(pc),a0
-		lea	$366ca,a1
-		move.w	#$4ef9,(a1)+
-		move.l	a0,(a1)			;Game Loader patched!
-		move.w	#$4e75,$3786c		;Remove Manual Protection
-		lea	no_save(pc),a0
-		move.l	a0,$30cd6
-		move.l	a0,$3134a
-		lea	do_save(pc),a0
-		move.l	a0,$30dc8
-		move.l	a0,$30df8
-		move.l	#$600000d6,$30ed8	;Remove FORMAT option!
+
 		lea	save(pc),a0
-loc:		lea	$80000,a1
-		bsr.s	_LoadFile
-		lea	logger_save(pc),a0
-		move.l	a0,$2eebc
-		lea	logger_load(pc),a0
-		move.l	a0,$2ee6a
-		
+		move.l	save_buffer(pc),a1
+		bsr	_LoadFile
+
+		IFND	RELOC_ENABLED
 		lea	depack(pc),a0
 		lea	$258be,a1
 		move.l	a1,a2
@@ -154,7 +174,16 @@ copydata2:
 		dbra	d0,copydata2
 		move.w	#$4ef9,(a2)+
 		move.l	a3,(a2)			;Patch depack!
-		jmp	$500.w
+		ENDC
+		
+		move.l	_resload(pc),a2
+		lea		pl_main(pc),a0
+		move.l	_reloc_base(pc),a1
+		move.l	a1,-(a7)
+		jsr		resload_Patch(a2)
+		add.l	#$500,(a7)
+		rts
+		
 ;----------------------------------
 logger_load:
 		movem.l	d0-d7/a0-a6,-(a7)
@@ -189,7 +218,7 @@ no_save:
 
 load_save:
 		movem.l	d0-d7/a0-a6,-(a7)
-		move.l	loc+2(pc),a1
+		move.l	save_buffer(pc),a1
 		moveq	#0,d0
 		move.w	d1,d0
 		moveq	#0,d1
@@ -211,9 +240,9 @@ saver:		move.b	(a0)+,(a1)+
 		dbra	d1,saver
 		move.l	(a7)+,a1
 		lea	save(pc),a0
-		move.l	loc+2(pc),a1
+		move.l	save_buffer(pc),a1
 		move.l	#$31800,d0
-		bsr.s	_SaveFile
+		bsr	_SaveFile
 skip_saver:
 		movem.l	(a7)+,d0-d7/a0-a6
 		moveq	#0,d0
@@ -229,7 +258,7 @@ Loader2:
 		moveq	#2,d2
 		mulu	#$200,d0
 		mulu	#$200,d1		
-		bsr.s	_LoadDisk
+		bsr	_LoadDisk
 		movem.l	(a7)+,d0-d7/a0-a6
 		rts
 Loader:
@@ -241,7 +270,7 @@ Loader:
 		moveq	#1,d2
 		mulu	#$200,d0
 		mulu	#$200,d1		
-		bsr.s	_LoadDisk
+		bsr	_LoadDisk
 		cmp.l	#$10000,a0
 		beq.s	wait
 		cmp.l	#$500,a0
@@ -257,7 +286,22 @@ wait		cmp.b	$dff006,d1
 skip:		movem.l	(a7)+,d0-d7/a0-a6
 		moveq	#0,d0
 		rts
-		
+	
+pl_main
+	PL_START
+	PL_CB	$14878			;Fix Address Error
+	PL_P	$366ca,Loader2	;Game Loader patched!
+	PL_R	$3786c		;Remove Manual Protection
+	PL_PS	$30cd4,no_save
+	PL_PS	$31348,no_save
+	PL_PS	$30dc6,do_save
+	PL_PS	$30df6,do_save
+	PL_L	$30ed8,$600000d6	;Remove FORMAT option!
+	PL_P	$2eeba,logger_save
+	PL_PS	$2ee68,logger_load
+	PL_END
+	
+	
 ;--------------------------------
 
 _resload	dc.l	0		;address of resident loader
@@ -288,13 +332,17 @@ _checkfile:
 		movem.l	(a7)+,d1/a0-a2
 		rts
 speedup2:
-		dcb.b	$1bb34-$1b8fc
+		ds.b	$1bb34-$1b8fc
 speedup:
-		dcb.b	$20714-$20518
+		ds.b	$20714-$20518
 depack:
-		dcb.b	$2596e-$258be
+		ds.b	$2596e-$258be
 ;----------------------------------
 
+_reloc_base
+	dc.l	0
+save_buffer
+	dc.l	$80000
 	
 ;======================================================================
 
