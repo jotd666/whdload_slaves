b90efaaa90b2bde14b66a45c33e63a52cc70e9bf
Jean-François Fabre <jotd666@gmail.com>
2022-12-30 00:26:02 +0100

    updated Dungeon Master package for V3.6 with files

--- D/DungeonMaster/usr/source/DungeonMasterHD.s
+++ D/DungeonMaster/usr/source/DungeonMasterHD.s
@@ -68,17 +68,20 @@ CACHE
 IOCACHE = 10000
 STACKSIZE = 4000
 
-CHIP_ONLY
+; note to self: on V3, to match memory layout with CHIP_ONLY, the
+; data files must have "devs" in them. Official installation
+; omits it so it boots with a black screen but shifts the
+; disassemblies I made
+;CHIP_ONLY
 ; amount of memory available for the system
 	IFD	CHIP_ONLY
-CHIPMEMSIZE	= $140000
+CHIPMEMSIZE	= $100000
 FASTMEMSIZE	= $0000
 	ELSE
 CHIPMEMSIZE	= $80000
 FASTMEMSIZE	= $50000
 	ENDC
 	
-; CNFG.FTL contains trackdisk.device code that checks if disk is present
 ;============================================================================
 
 
@@ -114,13 +117,13 @@ DECL_VERSION:MACRO
 	dc.b	$A,0
 
 slv_CurrentDir		dc.b	"data",0
-slv_name		dc.b	"Portal"
+slv_name		dc.b	"Dungeon Master"
 			IFD		CHIP_ONLY
 			dc.B	" (debug/chip mode)"
 			ENDC
 			
 			dc.b	0
-slv_copy		dc.b	"1986 Activision",0
+slv_copy		dc.b	"1986-1992 FTL/Software Heaven",0
 slv_info		dc.b	"Adapted by JOTD",10,10
 		dc.b	"Version "
 		DECL_VERSION
@@ -128,10 +131,14 @@ slv_info		dc.b	"Adapted by JOTD",10,10
 
 _assign1:
 	dc.b	"DF0",0
+_dm_assign:
+	dc.b	"DungeonMaster",0
 
 
 slv_config:
-       dc.b    "C1:X:trainer no damage:0;"
+	dc.b	"BW;"
+    dc.b    "C1:X:trainer no damage:0;"
+    dc.b    "C5:L:save slot:0,1,2,3,4,5,6,7,8,9:0;"
 	dc.b	0
 
 _procname:
@@ -160,7 +167,11 @@ _bootdos
 		lea	(_tag,pc),a0
 		jsr	(resload_Control,a2)
 		
-
+		; set slot selected by CUSTOM5
+		lea	_savegame_char(pc),a0
+		move.l	_savegame_slot(pc),d0
+		add.b	#'0',d0
+		move.b	d0,(a0)
 
 	;open doslib
 		lea	(_dosname,pc),a1
@@ -170,6 +181,9 @@ _bootdos
 
 	;assigns
 		lea	_assign1(pc),a0
+		sub.l	a1,a1
+		bsr	_dos_assign
+		lea	_dm_assign(pc),a0
 		sub.l	a1,a1
 		bsr	_dos_assign
 
@@ -270,61 +284,14 @@ _patchexe
 	add.l	a1,a1
 	add.l	a1,a1
 	addq.w	#4,a1	; first segment
-	lea loader_routine(pc),a0
-	move.l  ($82A,a1),(a0)
 	
 	move.l	_resload(pc),a2
 	lea		pl_bjeload(pc),a0
 	jsr		resload_Patch(a2)
 	
 	rts
-
-
-loader_routine_wrap
-	MOVE.L	-18(A5),-(A7)		;0820: 2f2dffee: file id or such
-	MOVE.L	-26(A5),-(A7)		;0824: 2f2dffe6: source address
-	move.l  loader_routine(pc),a0
-    jsr (a0)
-    movem.l a1,-(a7)
-    move.l  8(a7),a1
-    add.l   #$fbd6-$3154,a1
-    ; A0 is the start address
-    ; check if main engine is loaded
-    cmp.l   #$39460034,(a1)
-    bne.b   .nomain
-    move.l  8(a7),a1
-    movem.l d0-d1/A0/A2,-(a7)
-    move.l  _resload(pc),a2
-    lea pl_main(pc),a0
-    jsr resload_Patch(a2)
-    movem.l (a7)+,d0-d1/A0/A2
-.nomain
-    movem.l (a7)+,a1
     
-	ADDQ.W	#8,A7			;082e: 504f
-    rts
-    
-loader_routine
-    dc.l    0
-    
-pl_main
-	PL_START
-    PL_IFC1X    0
-    PL_NOP      $fbd6-$3154,4   ; no damage when hitting walls
-    PL_ENDIF
-    PL_END
-	
-pl_bjeload
-	PL_START
-	PL_PS	$6C,end_bje_patch
-	PL_PS	$E8C,main_jump
-	PL_P	$19BA,c_open
-	PL_P	$197A,c_create_proc
-    PL_PSS  $0820,loader_routine_wrap,10
-    PL_B    $084c,$60 ; remove checksum check so we can modify the main proggy...
-    PL_B    $0752,$60 ; remove checksum check so we can modify the main proggy...
-    PL_R    $0be0   ; remove checksum routine to save time!
-	PL_END
+   
 	
 ; don't know if it's any use...
 end_bje_patch
@@ -357,9 +324,21 @@ c_open:
 last_opened_file:
 	dc.l	0
 
+buttonwait
+.loop
+	btst	#6,$bfe001
+	beq.b	.out
+	btst	#7,$bfe001
+	beq.b	.out
+	bra.b	.loop
+.out
+	rts
+	
 ; for V3.6
+; called each time a ".FTL" file is loaded
+; runs it
 
-main_jump:
+subprogram_jump:
 	movem.l	D0-D1/A0-A2,-(A7)
 	move.l	_resload(pc),a2
 	
@@ -367,23 +346,30 @@ main_jump:
 
 	move.l	last_opened_file(pc),d0
 	cmp.l	#'KAOS',D0
-	bne.b	.nomain
+	bne.b	.nokaos
 
-	;;bsr	_replace_df0
-	nop
+	move.l	_buttonwait(pc),d0
+	beq.b	.no_bw
+	bsr		buttonwait
+.no_bw
+	move.l	a0,a1
+	lea		pl_kaos(pc),a0
+	jsr		resload_Patch(a2)
 	bra.b	.out
-.nomain
+.nokaos
 	cmp.l	#'CNFG',d0
 	bne.b	.noconfig
+	; just before "FTL" swoosh logo
+	; need to fix drive access
 	move.l	a0,a1
 	lea		pl_cnfg(pc),a0
 	jsr		resload_Patch(a2)
-	bra.b	.out
+;	bra.b	.out
 .noconfig
-	cmp.l	#'APPB',d0
-	bne.b	.noappb
-	blitz
-.noappb
+;	cmp.l	#'APPB',d0
+;	bne.b	.noappb
+;	
+;.noappb
 
 .out
 	bsr	_flushcache
@@ -391,17 +377,114 @@ main_jump:
 
 	jmp	(A0)
 	
+set_disk_name
+	move.l	4(a7),a0
+	move.l	a1,-(a7)
+	lea		_dm_assign(pc),a1
+	moveq	#0,d0
+.copy
+	move.b	(a1)+,(a0)+
+	beq.b	.out
+	addq	#1,d0
+	bra.b	.copy
+.out
+	move.l	(a7)+,a1
+	
+ 	MOVE.W	D0,D7			;3c3d6: 3e00
+	MOVEQ	#0,D0			;3c3d8: 7000
+	MOVE.W	D7,D0			;3c3da: 3007
+	rts
+	
+pl_kaos
+	PL_START
+    PL_P      $406b8-$3a4ee,check_disk_in_drive
+	;;PL_PS	$3c3d2-$3a4ee,set_disk_name
+	PL_PSS	$3c486-$3a4ee,check_main_disk_for_savegame,2
+	PL_PS	$5ba3e-$3a4ee,check_main_disk
+	; don't delete backup file
+	PL_NOP	$5c00e-$3a4ee,4
+	; don't rename backup to save file
+	; (I don't know when it should happen but if it does
+	; it will crash the game so it has to go)
+	PL_NOP	$5ce6c-$3a4ee,4
+	; don't rename current save file to backup
+	; whdload doesn't support Rename and it crashes kickemu
+	; at this point so skip it
+	PL_NOP	$5c01a-$3a4ee,4
+	; set proper savegame slot from "resume"
+	; (main disk not checked when quit+restart)
+	PL_PS	$5cb12-$3a4ee,set_savegame_slot_from_resume_v3
+	; trainer
+    PL_IFC1X    0
+    PL_NOP      $47c56-$3a4ee,4   ; no damage when hitting walls
+    PL_ENDIF
+    PL_END
+	
+pl_bjeload
+	PL_START
+	PL_PS	$6C,end_bje_patch
+	PL_PS	$E8C,subprogram_jump
+	PL_P	$19BA,c_open
+	PL_P	$197A,c_create_proc
+    ;;PL_PSS  $0820,loader_routine_wrap,10
+    PL_B    $084c,$60 ; remove checksum check so we can modify the main proggy...
+    PL_B    $0752,$60 ; remove checksum check so we can modify the main proggy...
+    PL_R    $0be0   ; remove checksum routine to save time!
+	PL_NOP	$0a68,6	; remove delay at startup
+	PL_END
+	
 pl_cnfg
 	PL_START
 	; skip useless "disk in drive" check
-	PL_NOP	$250a0-$24c4a,6
-	PL_B	$250a6-$24c4a,$60
+	;PL_NOP	$250a0-$24c4a,6
+	;PL_B	$250a6-$24c4a,$60
+	PL_P	$24f9a-$24c4a,check_disk_in_drive	; D0 != 0: disk in drive
 	PL_END
+	
+	
+check_main_disk
+	; check if main disk is inserted
+	; at the same time, change savegame filename to put
+	; our savename instead, from now on game uses that pointer
+	; (done there because it's called at game start)
+	bsr		set_proper_savegame_slot_v3
+	
+	moveq.l	#0,d0	; main disk inserted
+	move.w	d0,-16446(A5)
+	rts
+	
+set_savegame_slot_from_resume_v3
+	; original code
+	JSR	154(A5)			;5cb12: 4ead009a
+	SUBQ.W	#1,D0			;5cb16: 5340
+	bsr		set_proper_savegame_slot_v3
+	tst.w	d0
+	rts
+	
+set_proper_savegame_slot_v3
+	movem.l	A1,-(a7)
+	lea		_savegame_name(pc),a1
+	move.l	a1,-9084(A5)
+	movem.l	(a7)+,A1
+	rts
+	
+check_main_disk_for_savegame
+	moveq	#1,d0		; save disk inserted
+	move.w	d0,-16446(A5)
+	MOVE.W	D0,-2(A6)		;3c48a: 3d40fffe
+	rts
+	
+check_disk_in_drive
+	moveq	#1,d0
+	rts
 	
 _tag		dc.l	WHDLTAG_CUSTOM1_GET
 _custom1	dc.l	0
+		dc.l	WHDLTAG_BUTTONWAIT_GET
+_buttonwait	dc.l	0
 		dc.l	WHDLTAG_CUSTOM5_GET
-_skip_intro	dc.l	0
+_savegame_slot
+		dc.l	0
 		dc.l	0
 _seglist:
 	dc.l	0
@@ -415,4 +498,8 @@ _bjeload_seglist
 	dc.l	0
 _gfxname
 	dc.b	"graphics.library",0
+_savegame_name
+	dc.b	"save/DMGAME_"
+_savegame_char
+	dc.b	"0.DAT",0
 	
