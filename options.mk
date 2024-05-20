263789e0764d522961eb3710305b7602502cf263
Jean-François Fabre <jotd666@gmail.com>
2024-05-20 09:12:01 +0200

    started unifying makefiles

---
+++ options.mk
@@ -0,0 +1,12 @@
+# compute this dir, knowing that this file is included from a makefile
+# which is 2 levels below (letter/game). I haven't found a better way...
+THIS_DIR := $(dir $(abspath $(firstword $(MAKEFILE_LIST))))/../../
+
+# change to your includes base
+HDBASE = K:\jff\AmigaHD\amiga39_JFF_OS\include
+# unpack whdload dev in this dir at start
+WHDBASE = $(THIS_DIR)
+
+# standard date gen & build commands
+WDATE = wdate.py> datetime
+VASM = vasmm68k_mot -DDATETIME -I$(HDBASE) -I$(WHDBASE) -pic -devpac -nosym -Fhunkexe
